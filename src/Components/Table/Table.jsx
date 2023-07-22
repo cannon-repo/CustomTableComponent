6f4bbf41994c75b005716fb945c84ae42bd5f942
Prerit <preritcs2019@gmail.com>
2023-07-23 03:30:43 +0530

    added basic table structure

--- src/Components/Table/Table.jsx
+++ src/Components/Table/Table.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
-import "./Table.scss";
+import React from "react";
 
-const Table = () => {
+const Table = ({ data }) => {
   return (
-    <div className='Table'>Table</div>
-  )
-}
+    <table className="Table">
+      <thead>
+        <tr>
+          {data.length > 0 && Object.keys(data[0]).map((key, index) => <th key={index}>{key}</th>)}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((item, index) => (
+          <tr key={index}>
+            {Object.values(item).map((value, index) => (
+              <td key={index}>{value}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
-export default Table
+export default Table;
--- src/index.js
+++ src/index.js
@@ -3,9 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import Table from './Components/Table/Table';
 
+const data = [
+  { id: 1, name: 'John Doe', age: 30, occupation: 'Engineer', department: 'IT', location: 'New York' },
+  { id: 2, name: 'Jane Smith', age: 25, occupation: 'Designer', department: 'Design', location: 'Los Angeles' },
+  { id: 3, name: 'Bob Johnson', age: 40, occupation: 'Manager', department: 'HR', location: 'Chicago' },
+  { id: 4, name: 'Alice Williams', age: 28, occupation: 'Software Developer', department: 'IT', location: 'San Francisco' },
+  { id: 5, name: 'Michael Brown', age: 35, occupation: 'Product Manager', department: 'Product', location: 'Seattle' },
+  { id: 6, name: 'Emily Davis', age: 32, occupation: 'UX/UI Designer', department: 'Design', location: 'Boston' },
+  { id: 7, name: 'Alex Martinez', age: 27, occupation: 'Data Analyst', department: 'Analytics', location: 'Austin' },
+  { id: 8, name: 'Sophia Lee', age: 38, occupation: 'Finance Manager', department: 'Finance', location: 'San Diego' },
+  { id: 9, name: 'William Wilson', age: 33, occupation: 'Sales Executive', department: 'Sales', location: 'Denver' },
+  { id: 10, name: 'Olivia Miller', age: 29, occupation: 'Marketing Specialist', department: 'Marketing', location: 'Miami' },
+];
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Table />
+    <Table data={data} />
   </React.StrictMode>
 );
